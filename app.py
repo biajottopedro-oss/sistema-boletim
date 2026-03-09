df446f484ddc93a62ab0899a9b1367d044d1befc
Pedro Biajotto <biajottopedro@gmail.com>
2026-03-08 23:53:34 -0300

    sistema de login

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+usuario_correto = "professor"
+senha_correta = "1234"
+
 @app.route("/")
-def inicio():
+def login():
     return render_template("index.html")
+
+@app.route("/entrar", methods=["POST"])
+def entrar():
+
+    usuario = request.form["usuario"]
+    senha = request.form["senha"]
+
+    if usuario == usuario_correto and senha == senha_correta:
+        return render_template("painel.html")
+    else:
+        return "<h2>Usuário ou senha incorretos</h2>"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
